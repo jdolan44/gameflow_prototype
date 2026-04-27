be7c073accbe687e16cee2af3b5a78addd62f2a7
jdolan44 <Jdolan16646@gmail.com>
2026-04-27 15:55:20 -0400

    react app for simple game

---
+++ src/demos/react/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { createServer as createViteServer } from "vite";
+import { createServer as createHttpServer } from "http";
+import { createGameServer } from "gameflow";
+import { TicTacToe } from "../../games/TicTacToe.js";
+import { SimpleGame } from "../../games/SimpleGame.js";
+
+async function start() {
+    const app = express();
+
+    const vite = await createViteServer({
+        server: { middlewareMode: true }
+    });
+
+    app.use(vite.middlewares);
+
+    const gameRegistry = {
+        ticTacToe: TicTacToe,
+        simple: SimpleGame
+    };
+    const mycors = { origin: "*" };
+
+    const httpServer = createHttpServer(app);
+    createGameServer({ httpServer, gameRegistry, cors: mycors });
+
+    httpServer.listen(3000);
+}
+
+start();
--- src/demos/react/src/App.jsx
+++ src/demos/react/src/App.jsx
@@ -1,31 +1,41 @@
 import { Client } from 'gameflow-client'
 import './App.css'
+import { useEffect, useState } from 'react';
 
 // Create client OUTSIDE the component to prevent multiple socket connections
 const client = new Client("http://localhost:3000");
 
-// Register handlers ONCE outside the component
-client.onJoin((data) => {
-  console.log(data);
-});
-
-client.onMyTurn(() => {
-  let turn = window.prompt("what's your turn?");
-  client.takeTurn(turn);
-});
-
-client.onGameOver((outcome) => {
-  switch (outcome.reason) {
-    case "winner": handleWinner(outcome.isWinner); break;
-    case "quit": handleQuit(outcome.quitter); break;
-    case "draw": handleDraw(); break;
-  }
-});
-
 function App() {
+  const [move, setMove] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    client.onJoin((data) => {
+      setStatus(data.status);
+    });
+
+    client.onMyTurn(() => {
+      setStatus("it's your turn!");
+    });
+
+    client.onGameOver((outcome) => {
+      switch (outcome.reason) {
+        case "winner": handleWinner(outcome.isWinner, setStatus); break;
+        case "quit": handleQuit(outcome.quitter); break;
+        case "draw": handleDraw(); break;
+      }
+    });
+  })
+
   return (
     <>
-      <button onClick={() => joinGame(client)}>Join Game</button>
+      <div>
+        <div>Simple Game</div><br />
+        <button onClick={() => joinGame(client)}>Join Game</button><br />
+        <div id="status">{status}</div>
+        <input type="text" value={move} onChange={(e) => setMove(e.target.value)}></input>
+        <button onClick={() => client.takeTurn(move)}>Submit</button>
+      </div>
     </>
   )
 }
@@ -34,17 +44,17 @@ function joinGame(client) {
   client.joinGame("simple");
 }
 
-function handleWinner(isWinner) {
-  if (isWinner) window.alert("you win!");
-  else window.alert("you lose!");
+function handleWinner(isWinner, setStatus) {
+  if (isWinner) setStatus("you win!");
+  else setStatus("you lose!");
 }
 
-function handleQuit(quitter) {
-  window.alert("game quit by " + quitter);
+function handleQuit(quitter, setStatus) {
+  setStatus("game quit by " + quitter);
 }
 
-function handleDraw() {
-  window.alert("it's a draw!");
+function handleDraw(setStatus) {
+  setStatus("it's a draw!");
 }
 
 export default App
