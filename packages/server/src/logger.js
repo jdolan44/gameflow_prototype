e95acc1aa5570a137950873126ccf091e290fcf7
jdolan44 <Jdolan16646@gmail.com>
2026-05-05 22:53:24 -0400

    add config for logging and optional file exports

--- packages/server/src/logger.js
+++ packages/server/src/logger.js
@@ -1,18 +1,41 @@
 import winston from 'winston';
-const { combine, timestamp, colorize, printf } = winston.format;
+import config from "../config/logger.json" with {type: "json"}
+const { combine, timestamp, colorize, printf, json } = winston.format;
 
-let level = "silly";
+let transports = []
+
+
+//custom timestamp
+let defaultTimestamp = timestamp({
+    format: 'YYYY-MM-DD hh:mm:ss.SSS A',
+});
+
+//custom formats
+let consoleDefaultFormat = combine(
+    colorize({ all: true }),
+    defaultTimestamp,
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message} ${JSON.stringify(info.meta)}`)
+)
+
+//enable console if needed
+if (config.console.enabled) {
+    transports.push(new winston.transports.Console({
+        level: config.console.level,
+        format: consoleDefaultFormat
+    }));
+}
+//enable file logging
+if (config.file.enabled) {
+    transports.push(new winston.transports.File({
+        level: config.file.level,
+        filename: config.file.filename,
+        format: combine(defaultTimestamp, json())
+    }));
+}
 
 const logger = winston.createLogger({
-    level,
-    format: combine(
-        colorize({ all: true }),
-        timestamp({
-            format: 'YYYY-MM-DD hh:mm:ss.SSS A',
-        }),
-        printf((info) => `[${info.timestamp}] ${info.level}: ${info.message} ${JSON.stringify(info.meta)}`)
-    ),
-    transports: [new winston.transports.Console()],
+    silent: config.disableAll,
+    transports
 });
 
 /**
